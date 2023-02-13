7fdb283a0af276c7969daad233ca484340973ad4
Daniel Tedesco <dtedesco1@gmail.com>
2023-02-13 22:51:50 +0800

    Update video_upload.py

--- video_upload.py
+++ video_upload.py
@@ -31,3 +31,8 @@ def upload_to_youtube(video_file):
         print(f"Video was uploaded successfully with ID: {response['id']}")
     except HttpError as error:
         print(f"An error occurred while uploading the video: {error}")
+        
+        
+# In this script, video_file is the path to the final video file, and credentials.json is the path to the Google API Console project credentials file. The video's title, description, tags, and category ID are specified in the body parameter of the youtube.videos().insert() method. The privacy status is set to "public", meaning that the video will be visible to anyone.
+
+# Note that uploading a video to YouTube using the API can take a long time, especially for large video files. You should be prepared to handle timeouts and retry the upload if necessary. You may also want to consider using a service account for authentication, as opposed to an authorized user, to avoid the need for manual authentication.
